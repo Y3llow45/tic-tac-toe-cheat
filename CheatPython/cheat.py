3005a05c9b2eb263d88894cb9511728351652ec8
Борислав Христов <hristovborislav45@gmail.com>
2024-08-30 12:44:41 +0300

    Update cheat.py

--- CheatPython/cheat.py
+++ CheatPython/cheat.py
@@ -8,23 +8,40 @@ import time
 service = Service(executable_path='./driver/geckodriver.exe')
 driver = webdriver.Firefox(service=service)
 
-driver.get("https://www.google.com/search?q=tic+tac+toe")
+def main():
+  load()
+  setClickables()
+  bestFirstMove = driver.find_element(By.XPATH, "//td[@jsname='WfZakb' and @data-col='0']")
+  bestFirstMove.click()
+  time.sleep(2)
 
-WebDriverWait(driver, 10).until(
+  time.sleep(20)
+  driver.quit()
+
+def load():
+  driver.get("https://www.google.com/search?q=tic+tac+toe")
+  WebDriverWait(driver, 1).until(
     lambda d: d.execute_script("return document.readyState") == "complete"
-)
-time.sleep(6)
+  )
+  div = driver.find_element(By.CSS_SELECTOR, "div.QS5gu.sy4vM")
+  div.click()
+  time.sleep(2)
 
-bestFirstMove = driver.find_element(By.XPATH, "//td[@jsname='WfZakb' and @data-col='0']")
-bestFirstMove.click()
-time.sleep(2)
+def setClickables():
+  global restart_button
+  global btn1
+  global btn2
+  global btn3
+  global btn4
+  global btn5
+  global btn6
+  global btn7
+  global btn8
+  global btn9
+  restart_button = driver.find_elements(By.CLASS_NAME, 'lv7K9c')[2]
 
-elements = driver.find_elements(By.CLASS_NAME, '')
-if len(elements) >= 3:
-  elements[2].click()
-else:
-  print("Less than three elements found.")
+def restart():
+  restart_button.click()
 
-print('clicked restart')
-time.sleep(20)
-driver.quit()
+if __name__ == "__main__":
+  main()
