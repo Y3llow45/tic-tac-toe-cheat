604cf00cf876bf6199a6c59d673f17664e4a4bf9
Борислав Христов <hristovborislav45@gmail.com>
2024-08-30 11:29:25 +0300

    Update python cheat

--- CheatPython/cheat.py
+++ CheatPython/cheat.py
@@ -13,8 +13,18 @@ driver.get("https://www.google.com/search?q=tic+tac+toe")
 WebDriverWait(driver, 10).until(
     lambda d: d.execute_script("return document.readyState") == "complete"
 )
-time.sleep(10)
+time.sleep(6)
 
-corner = driver.find_element(By.XPATH, "//td[@jsname='WfZakb']")
-corner.click()
-#driver.quit()
+bestFirstMove = driver.find_element(By.XPATH, "//td[@jsname='WfZakb' and @data-col='0']")
+bestFirstMove.click()
+time.sleep(2)
+
+elements = driver.find_elements(By.CLASS_NAME, '')
+if len(elements) >= 3:
+  elements[2].click()
+else:
+  print("Less than three elements found.")
+
+print('clicked restart')
+time.sleep(20)
+driver.quit()
