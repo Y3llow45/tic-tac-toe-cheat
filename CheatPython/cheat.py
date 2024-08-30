9597dc305f0b852dda4df01ab6e63653192aefc8
Борислав Христов <hristovborislav45@gmail.com>
2024-08-30 10:51:01 +0300

    Use firefox instead

--- CheatPython/cheat.py
+++ CheatPython/cheat.py
@@ -3,9 +3,9 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# Path to geckodriver
-service = Service(executable_path='/path/to/geckodriver')  # Update this path
+service = Service(executable_path='./driver/geckodriver.exe')
 driver = webdriver.Firefox(service=service)
 
 driver.get("https://www.google.com/search?q=tic+tac+toe")
@@ -13,8 +13,8 @@ driver.get("https://www.google.com/search?q=tic+tac+toe")
 WebDriverWait(driver, 10).until(
     lambda d: d.execute_script("return document.readyState") == "complete"
 )
+time.sleep(10)
 
 corner = driver.find_element(By.XPATH, "//td[@jsname='WfZakb']")
 corner.click()
-
-driver.quit()
+#driver.quit()
