7b4d3a8aebca96346f74d9a6ff5fe9d899097af6
Борислав Христов <hristovborislav45@gmail.com>
2024-09-01 15:41:39 +0300

    Update cheat.py

--- CheatPython/cheat.py
+++ CheatPython/cheat.py
@@ -7,11 +7,20 @@ import time
 
 service = Service(executable_path='./driver/geckodriver.exe')
 driver = webdriver.Firefox(service=service)
+X = 'X'
+O = 'O'
+EMPTY = ' '
+board = [[EMPTY for _ in range(3)] for _ in range(3)]
 
 def main():
   load()
   setClickables()
-  btns[2].click()
+  btns[0].click()
+  # check opponent selection
+  # Minimax() 3x3 list of lists
+  time.sleep(2)
+  update_board()
+  print(board)
   time.sleep(20)
   driver.quit()
 
@@ -40,6 +49,23 @@ def setClickables():
   ))
   if len(restart_buttons) > 2:
     restart_button = restart_buttons[2]
+
+def update_board():   
+  for i in range(3):
+    for j in range(3):
+      cell = driver.find_element(By.XPATH, f"//td[@jsname='WfZakb' and @data-col='{j}' and @data-row='{i}']")
+         
+      x_element = cell.find_element(By.CLASS_NAME, "uECznc")
+      o_element = cell.find_element(By.CLASS_NAME, "D7yUae")
+            
+      if x_element.is_displayed():
+        board[i][j] = X
+      elif o_element.is_displayed():
+        board[i][j] = O
+      else:
+        board[i][j] = EMPTY
+    return board
+
 
 def restart():
   restart_button.click()
